3532813f135cca8a6aedb30e25fd52267e9c663c
willpenter <98774430+willpenter@users.noreply.github.com>
2024-02-15 17:02:27 -0500

    Update takeoff.py

--- takeoff.py
+++ takeoff.py
@@ -1,12 +1,9 @@
 from djitellopy import Tello
-
+#This code just takes off and lands, usable for flight test
 tello = Tello()
 
 tello.connect()
 tello.takeoff()
 
-tello.move_left(50)
-tello.rotate_clockwise(50)
-tello.move_forward(50)
 
 tello.land()
